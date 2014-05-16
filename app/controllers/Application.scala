3499095f820900e56ba63d19aa7c87ffb4651798
Long Cao <longcao@gmail.com>
2014-05-16 00:31:07 -0400

    /blog route and action

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -11,6 +11,11 @@ object Application extends Controller {
     Ok(views.html.master(content))
   }
 
+  def blog = Action {
+    val content: Html = views.html.content()
+    Ok(views.html.master(content))
+  }
+
   def about = Action {
     val content: Html = views.html.about()
     Ok(views.html.master(content))

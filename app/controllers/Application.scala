d36d0cf5b74f3c679c04240da5c307aa6b03b6f5
Long Cao <longcao@gmail.com>
2014-05-31 23:34:41 -0400

    Added basic permalink capability - use Html.empty instead of placeholder content - notfound template

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,18 +12,12 @@ import securesocial.core.SecureSocial
 object Application extends Controller with SecureSocial {
 
   def home = Action {
-    val content: Html = views.html.content()
-    Ok(views.html.master(content))
-  }
-
-  def blog = Action {
-    val content: Html = views.html.content()
-    Ok(views.html.master(content))
+    Ok(views.html.master(Html.empty))
   }
 
   def about = Action {
-    val content: Html = views.html.about()
-    Ok(views.html.master(content))
+    val about: Html = views.html.about()
+    Ok(views.html.master(about))
   }
 
   def editor = SecuredAction {
@@ -31,10 +25,23 @@ object Application extends Controller with SecureSocial {
     Ok(views.html.master(editor))
   }
 
+  def blog = Action {
+    Ok(views.html.master(Html.empty))
+  }
+
   def save = Action(parse.json) { implicit request =>
     val post: Post = request.body.as[Post]
     val savedPost: Post = PostRepository.save(post)
     Ok(Json.toJson(savedPost).toString)
+  }
+
+  def permalink(id: Long) = Action {
+    PostRepository.findById(Option(id)) match {
+      case Some(post: Post) =>
+        val permalink = views.html.blog.permalink(post)
+        Ok(views.html.master(permalink))
+      case _ => NotFound(views.html.master(views.html.notfound()))
+    }
   }
 
 }

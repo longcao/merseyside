b9f54b9daba21b46361f9f9e7f3bcd46bac53580
Long Cao <longcao@gmail.com>
2014-06-21 00:41:54 -0400

    separate blogcontroller out into traits

--- app/controllers/BlogController.scala
+++ app/controllers/BlogController.scala
@@ -3,13 +3,17 @@ package controllers
 import models.{ Post, PostRepository }
 
 import play.api._
+import play.api.http.ContentTypes.JSON
+import play.api.http.HeaderNames.CONTENT_TYPE
 import play.api.libs.json.Json
 import play.api.mvc._
 import play.api.templates.Html
 
 import securesocial.core.SecureSocial
 
-object BlogController extends Controller with SecureSocial {
+trait BlogController extends Controller
+  with SecureSocial
+  with BlogCrud {
 
   def editor = SecuredAction {
     val editor: Html = views.html.editor()
@@ -18,12 +22,6 @@ object BlogController extends Controller with SecureSocial {
 
   def blog = Action {
     Ok(views.html.master(Html.empty))
-  }
-
-  def save = SecuredAction(ajaxCall = true)(parse.json) { implicit request =>
-    val post: Post = request.body.as[Post]
-    val savedPost: Post = PostRepository.save(post)
-    Ok(Json.toJson(savedPost).toString)
   }
 
   def permalink(id: Long) = Action {
@@ -36,3 +34,16 @@ object BlogController extends Controller with SecureSocial {
   }
 
 }
+
+trait BlogCrud {
+  self: BlogController =>
+
+  def save = SecuredAction(ajaxCall = true)(parse.json) { implicit request =>
+    val post: Post = request.body.as[Post]
+    val savedPost: Post = PostRepository.save(post)
+    Ok(Json.toJson(savedPost).toString)
+      .withHeaders(CONTENT_TYPE -> JSON)
+  }
+}
+
+object BlogController extends BlogController

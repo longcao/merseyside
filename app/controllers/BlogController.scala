af35e3bb6c11f5c26d87b9d19fb12cba3771aef1
Long Cao <longcao@gmail.com>
2015-04-22 01:24:21 -0400

    sort blog frontpage by newest first

--- app/controllers/BlogController.scala
+++ app/controllers/BlogController.scala
@@ -14,7 +14,7 @@ import service.PostService
 object BlogController extends Controller {
 
   def frontpage = Action.async { request =>
-    val posts = PostService.posts.values.toList
+    val posts = PostService.posts.values.toList.sorted(Post.postOrdering)
     val frontpage = views.html.blog.frontpage(posts)
     Future.successful(Ok(views.html.master(frontpage)))
   }
--- app/model/Post.scala
+++ app/model/Post.scala
@@ -2,6 +2,12 @@ package model
 
 import org.joda.time.LocalDate
 
+object Post {
+  implicit val postOrdering: Ordering[Post] = new Ordering[Post] {
+    def compare(p1: Post, p2: Post): Int = p2.date.compareTo(p1.date)
+  }
+}
+
 case class Post(
   title: String,
   date: LocalDate,

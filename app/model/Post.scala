87c0e71db581b0863c647a3bcf950955b0c7bbe4
Long Cao <longcao@gmail.com>
2015-06-24 00:12:41 -0400

    rfc 822 and rfc 3339 date formats, default to a time for now

--- app/model/Post.scala
+++ app/model/Post.scala
@@ -1,13 +1,14 @@
 package model
 
 import org.joda.time.format.DateTimeFormat
-import org.joda.time.LocalDate
+import org.joda.time.{ DateTime, DateTimeZone, LocalDate }
 
 object Post {
   implicit val postOrdering: Ordering[Post] = new Ordering[Post] {
     def compare(p1: Post, p2: Post): Int = p2.date.compareTo(p1.date)
   }
-  val rfc822format = DateTimeFormat.forPattern("EEE, dd MMM yyyy")
+  val rfc822format = DateTimeFormat.forPattern("EEE, dd MMM yyyy HH:mm:ss Z")
+  val rfc3339format = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
 }
 
 case class Post(
@@ -17,8 +18,15 @@ case class Post(
   tags: Seq[String],
   content: String) {
 
+  // default to noon UTC since no timestamp on posts
+  val defaultDate: DateTime = date.toDateTimeAtStartOfDay(DateTimeZone.UTC)
+    .plusHours(12)
+
   // ex: Tue, 08 Jun 2015 12:00:00 GMT
-  def formattedDate: String = s"${Post.rfc822format.print(date)} 12:00:00 GMT"
+  def formattedDateRFC822: String = s"${Post.rfc822format.print(defaultDate)}"
+
+  // ex: 2015-06-08T18:30:02Z
+  def formattedDateRFC3339: String = s"${Post.rfc3339format.print(defaultDate)}"
 
   // domain name and full path
   def fullPermalink: String = s"http://longcao.org$permalink"
